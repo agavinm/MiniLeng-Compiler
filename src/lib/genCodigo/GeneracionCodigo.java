58d7a27bcb68e8d8cc5da459eb2adb0a189b9775
agavinm <agavinm@gmail.com>
2019-06-05 12:10:15 +0200

    Generación de código: acciones

--- src/lib/genCodigo/GeneracionCodigo.java
+++ src/lib/genCodigo/GeneracionCodigo.java
@@ -6,7 +6,9 @@ import java.io.PrintWriter;
 public class GeneracionCodigo {
     private String fout_name;
     private String buffer;
-    private Integer num_etiqueta;
+    
+    private Integer num_etiqueta_pri;
+    private Integer num_etiqueta_fin;
     
     public GeneracionCodigo(String file) throws FicheroFormatoException {
         String[] splited = file.split("\\.");
@@ -16,17 +18,24 @@ public class GeneracionCodigo {
         
         fout_name = splited[0] + ".code";
         buffer = null;
-        num_etiqueta = null;
+        num_etiqueta_pri = null;
+        num_etiqueta_pri = null;
     }
     
     public void inicializar() {
-        num_etiqueta = -1; // Inicializar en uno menos al primero
+        num_etiqueta_pri = -1; // Inicializar en uno menos al primero
+        num_etiqueta_fin = -1;
         buffer = "";
     }
     
     public String nueva_etiqueta() {
-        num_etiqueta++;
-        return "L" + num_etiqueta;
+        num_etiqueta_pri++;
+        return "L" + num_etiqueta_pri;
+    }
+    
+    public String cerrar_etiqueta() {
+        num_etiqueta_fin++;
+        return "L" + num_etiqueta_fin;
     }
     
     public void escribir(String linea) {
